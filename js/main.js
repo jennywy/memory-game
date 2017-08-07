f55ec3e991580e6079c042a4cfa89f04e8ed9891
Jenny Yee <jennyyee@Jennys-MacBook-Air.local>
2017-08-07 15:22:49 -0400

    added js directory

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var cardOne = "Queen";
+var cardTwo = "Queen";
+var cardThree = "King";
+var cardFour= "King";
+console.log("User flipped " + cardFour);
+console.log("User flipped "+ cardTwo);
