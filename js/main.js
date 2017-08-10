0e3ff5c1ac2fbde1d229bff2889d6697ffe29ccc
Jenny Yee <jennyyee@Jennys-MacBook-Air.local>
2017-08-09 20:29:54 -0400

    modified js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
-console.log("Up and running!");
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour= "King";
-console.log("User flipped " + cardFour);
-console.log("User flipped "+ cardTwo);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards [0];
+cardsInPlay.push(cardOne);
+cardsInPlay;
+console.log("User flipped queen");
+var cardTwo = cards [2];
+cardsInPlay.push(cardTwo);
+cardsInPlay;
+console.log("User flipped king");
+
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) ? (alert("You found a match!") : alert("Sorry, try again");)
+}
+
+/*
+//this works, but without two else statements?
+if (cardsInPlay.length === 2) {
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again")}
+}
+// else ... stuff?? 
+*/ 
