82292fd4fc9b409d5310f5b2df027dadec0e3226
Jenny Yee <jennyyee@Jennys-MacBook-Air.local>
2017-08-09 20:32:43 -0400

    removed comments

--- js/main.js
+++ js/main.js
@@ -10,19 +10,13 @@ cardsInPlay;
 console.log("User flipped king");
 
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) ? (alert("You found a match!") : alert("Sorry, try again");)
-}
 
-/*
 //this works, but without two else statements?
 if (cardsInPlay.length === 2) {
 
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again")
+	}
 }
-else {
-	alert("Sorry, try again")}
-}
-// else ... stuff?? 
-*/ 
