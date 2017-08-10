01f202e0a9620bb4a2a75767ab130cb2bf9ddbc1
Jenny Yee <jennyyee@Jennys-MacBook-Air.local>
2017-08-10 17:08:28 -0400

    completed flipCard and checkForMatch game logic

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards [0];
-cardsInPlay.push(cardOne);
-cardsInPlay;
-console.log("User flipped queen");
-var cardTwo = cards [2];
-cardsInPlay.push(cardTwo);
-cardsInPlay;
-console.log("User flipped king");
 
-
-
-//this works, but without two else statements?
-if (cardsInPlay.length === 2) {
-
+var checkForMatch = function (){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again")
 	}
 }
+
+var flipCard = function (cardID) {
+console.log("User flipped "+ cards[cardID]);
+cardsInPlay.push(cards[cardID]);
+
+if (cardsInPlay.length === 2) {
+checkForMatch ();
+}
+
+
+}
+
+flipCard (0);
+flipCard (2);
